81de175552db6307829ab661b86c5d3a8e8bd51b
Atticus Kaphaem <66662081+Attagoat@users.noreply.github.com>
2026-04-27 14:21:03 -0500

    Project started

--- src/App.java
+++ src/App.java
@@ -2,4 +2,13 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    public static void readLine(){
+
+    }
+
+    public static boolean isLineValid(){
+        //TODO: stub 
+        return true;
+    }
 }
---
+++ src/project2/CPU.java
@@ -0,0 +1,8 @@
+package project2;
+import net.datastructures.*;
+public class CPU{
+    Queue<Process> processQueue;
+    Process runningProcess;
+
+    
+}
